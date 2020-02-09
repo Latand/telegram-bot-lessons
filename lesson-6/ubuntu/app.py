3c4cf890c430371ab3e526af3dec17a230e7c2e4
latand <latand666@gmail.com>
2020-02-09 14:41:09 +0200

    Багофикс

--- lesson-6/ubuntu/app.py
+++ lesson-6/ubuntu/app.py
@@ -19,7 +19,7 @@ async def on_startup(dp):
 
 
 if __name__ == '__main__':
-    from ubuntu.admin_panel import dp
-    from ubuntu.handlers import dp
+    from admin_panel import dp
+    from handlers import dp
 
     executor.start_polling(dp, on_shutdown=on_shutdown, on_startup=on_startup)
