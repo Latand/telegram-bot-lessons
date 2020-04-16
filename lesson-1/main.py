e93e8bd9d4c21ebc4b2857a611228e0174977b4f
Kostiantyn Kriuchkov <36363097+Latand@users.noreply.github.com>
2020-04-16 14:15:03 +0300

    Update main.py

--- lesson-1/main.py
+++ lesson-1/main.py
@@ -1,13 +1,11 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher, executor
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from config import BOT_TOKEN
 
 loop = asyncio.get_event_loop()
 bot = Bot(BOT_TOKEN, parse_mode="HTML")
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage, loop=loop)
+dp = Dispatcher(bot, loop=loop)
 
 if __name__ == '__main__':
     from handlers import dp
