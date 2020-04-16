6ac5539c7efa99d927a895dbaefeee4cdf4dc6d0
Kostiantyn Kriuchkov <36363097+Latand@users.noreply.github.com>
2020-04-16 14:24:00 +0300

    Update main.py

--- lesson-1/main.py
+++ lesson-1/main.py
@@ -3,9 +3,10 @@ import asyncio
 from aiogram import Bot, Dispatcher, executor
 from config import BOT_TOKEN
 
-loop = asyncio.get_event_loop()
+# loop = asyncio.get_event_loop()
+# Поток нам не нужен, т.к. он и так создается в диспатчере.
 bot = Bot(BOT_TOKEN, parse_mode="HTML")
-dp = Dispatcher(bot, loop=loop)
+dp = Dispatcher(bot)
 
 if __name__ == '__main__':
     from handlers import dp
