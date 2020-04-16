27681b4cba1e806ecd7920a09d48ad5206b3b262
Kostiantyn Kriuchkov <36363097+Latand@users.noreply.github.com>
2020-04-16 14:24:47 +0300

    Update loader.py

--- lesson-2-default-keyboard/loader.py
+++ lesson-2-default-keyboard/loader.py
@@ -1,13 +1,11 @@
 import logging
 
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
+dp = Dispatcher(bot)
 
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
                     level=logging.INFO,
