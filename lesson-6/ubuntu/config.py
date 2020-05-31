a1ca0d1c3b042e075ade205672c6348a300250ce
semenovsd <semenov_sd@bk.ru>
2020-05-31 19:49:30 +0300

    refact params
    Это к моему коммиту в дб

--- lesson-6/ubuntu/config.py
+++ lesson-6/ubuntu/config.py
@@ -3,10 +3,15 @@ from pathlib import Path
 
 TOKEN = os.getenv("TOKEN")
 admin_id = int(os.getenv("ADMIN_ID"))
-db_user = os.getenv("DB_USER")
-db_pass = os.getenv("DB_PASS")
+
+# DB settings
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+
+
 lp_token = os.getenv("LIQPAY_TOKEN")
-host = os.getenv("IP")
 
 I18N_DOMAIN = 'testbot'
 BASE_DIR = Path(__file__).parent
