c5538bcb17e072e14ef72960fb7032fa17a196f3
Justchisk <71386405+Justchisk@users.noreply.github.com>
2021-02-16 16:15:49 +0600

    Update load_all.py

--- lesson-5/ubuntu/load_all.py
+++ lesson-5/ubuntu/load_all.py
@@ -12,7 +12,7 @@ from sql import create_pool, create_db
 
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
                     level=logging.INFO)
-# loop = asyncio.get_event_loop()
+loop = asyncio.get_event_loop()
 # Поток нам не нужен, т.к. он и так создается в диспатчере.
 
 # Set up storage (either in Redis or Memory)
@@ -22,4 +22,4 @@ storage = MemoryStorage()
 bot = Bot(token=TOKEN, parse_mode="HTML")
 dp = Dispatcher(bot, storage=storage)
 
-db = dp.loop.run_until_complete(create_pool())
+db = loop.run_until_complete(create_pool())
