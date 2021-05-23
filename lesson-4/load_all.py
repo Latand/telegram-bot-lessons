8e1c4dd3c3c3978e376259391d860ff1e1e20ade
abdullaev388 <78722918+abdullaev388@users.noreply.github.com>
2021-05-23 15:36:52 +0500

    Update load_all.py

--- lesson-4/load_all.py
+++ lesson-4/load_all.py
@@ -1,4 +1,3 @@
-import asyncio
 import logging
 
 from aiogram import Bot
@@ -6,20 +5,16 @@ from aiogram import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from config import TOKEN
-from sql import create_pool
+from sql import Database
 
-# from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
                     level=logging.INFO)
-# loop = asyncio.get_event_loop()
-# Поток нам не нужен, т.к. он и так создается в диспатчере.
 
-# Set up storage (either in Redis or Memory)
 storage = MemoryStorage()
-# storage = RedisStorage2()
+
 
 bot = Bot(token=TOKEN, parse_mode="HTML")
 dp = Dispatcher(bot, storage=storage)
 
-db = dp.loop.run_until_complete(create_pool())
+db = Database() #Создаем обьект класса работы с бд из sql.py
