3a3a9a09b6b46951ef8fad01eb4a6a45c042d96f
abdullaev388 <78722918+abdullaev388@users.noreply.github.com>
2021-05-23 15:35:29 +0500

    Update handlers.py
    Вся работа с бд перенесена в sql.py

--- lesson-4/handlers.py
+++ lesson-4/handlers.py
@@ -1,75 +1,7 @@
 import random
 
 from aiogram import types
-from asyncpg import Connection, Record
-from asyncpg.exceptions import UniqueViolationError
-
-from load_all import bot, dp, db
-
-
-class DBCommands:
-    pool: Connection = db
-    ADD_NEW_USER_REFERRAL = "INSERT INTO users(chat_id, username, full_name, referral) " \
-                            "VALUES ($1, $2, $3, $4) RETURNING id"
-    ADD_NEW_USER = "INSERT INTO users(chat_id, username, full_name) VALUES ($1, $2, $3) RETURNING id"
-    COUNT_USERS = "SELECT COUNT(*) FROM users"
-    GET_ID = "SELECT id FROM users WHERE chat_id = $1"
-    CHECK_REFERRALS = "SELECT chat_id FROM users WHERE referral=" \
-                      "(SELECT id FROM users WHERE chat_id=$1)"
-    CHECK_BALANCE = "SELECT balance FROM users WHERE chat_id = $1"
-    ADD_MONEY = "UPDATE users SET balance=balance+$1 WHERE chat_id = $2"
-
-    async def add_new_user(self, referral=None):
-        user = types.User.get_current()
-
-        chat_id = user.id
-        username = user.username
-        full_name = user.full_name
-        args = chat_id, username, full_name
-
-        if referral:
-            args += (int(referral),)
-            command = self.ADD_NEW_USER_REFERRAL
-        else:
-            command = self.ADD_NEW_USER
-
-        try:
-            record_id = await self.pool.fetchval(command, *args)
-            return record_id
-        except UniqueViolationError:
-            pass
-
-    async def count_users(self):
-        record: Record = await self.pool.fetchval(self.COUNT_USERS)
-        return record
-
-    async def get_id(self):
-        command = self.GET_ID
-        user_id = types.User.get_current().id
-        return await self.pool.fetchval(command, user_id)
-
-    async def check_referrals(self):
-        user_id = types.User.get_current().id
-        command = self.CHECK_REFERRALS
-        rows = await self.pool.fetch(command, user_id)
-        return ", ".join([
-            f"{num + 1}. " + (await bot.get_chat(user["chat_id"])).get_mention(as_html=True)
-            for num, user in enumerate(rows)
-        ])
-
-    async def check_balance(self):
-        command = self.CHECK_BALANCE
-        user_id = types.User.get_current().id
-        return await self.pool.fetchval(command, user_id)
-
-    async def add_money(self, money):
-        command = self.ADD_MONEY
-        user_id = types.User.get_current().id
-        return await self.pool.fetchval(command, money, user_id)
-
-
-db = DBCommands()
-
+from load_all import db, dp
 
 @dp.message_handler(commands=["start"])
 async def register_user(message: types.Message):
@@ -84,7 +16,7 @@ async def register_user(message: types.Message):
     else:
         text += "Записал в базу! "
 
-    bot_username = (await bot.me).username
+    bot_username = (await dp.bot.me).username
     bot_link = f"https://t.me/{bot_username}?start={id}"
     balance = await db.check_balance()
     text += f"""
@@ -98,12 +30,12 @@ async def register_user(message: types.Message):
 Добавить монет: /add_money
 """
 
-    await bot.send_message(chat_id, text)
+    await dp.bot.send_message(chat_id, text)
 
 
 @dp.message_handler(commands=["referrals"])
 async def check_referrals(message: types.Message):
-    referrals = await db.check_referrals()
+    referrals = await db.check_referrals(dp)
     text = f"Ваши рефералы:\n{referrals}"
 
     await message.answer(text)
@@ -120,3 +52,4 @@ async def add_money(message: types.Message):
 Теперь ваш баланс: {balance}
     """
     await message.answer(text)
+
